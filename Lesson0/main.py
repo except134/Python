1e86402ebdb994d8d89610377a55c4929727cab9
except <except134@gmail.com>
2024-06-12 02:10:23 +0300

    Lesson0 added

---
+++ Lesson0/main.py
@@ -0,0 +1,30 @@
+# Task 1
+a = "Hello, world!!!"
+print(len(a))
+
+# Task 2
+first = 10
+second = 20
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+
+# Task 3
+first = 10
+second = 20
+third = 30
+mean = (first + second + third) / 3
+print(mean)
+
+# Task 4
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string + ", " + first_string)
+
+# Task 5
+a = 2
+b = 3
+c = 4
+f = (a * b) + (a * c)
+print(f**3 / 2)
