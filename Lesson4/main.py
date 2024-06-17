5cc55d9d0d211ab059a963fd99fd978493e20181
except <except134@gmail.com>
2024-06-17 07:24:58 +0300

    Added Lesson 4

---
+++ Lesson4/main.py
@@ -0,0 +1,8 @@
+my_string = input("Введите любой текст: ")
+print("Длина введенной строки:", len(my_string))
+print("Cтрока в верхнем регистре:", my_string.upper())
+print("Cтрока в нижнем регистре:", my_string.lower())
+print("Cтрока без пробелов:", my_string.replace(" ", ""))
+print("Первый символ строки:", my_string[0])
+print("Последний символ строки:", my_string[-1])
+
