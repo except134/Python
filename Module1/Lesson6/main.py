2a28ceff8e4d30c27f91e98961d1d82036f9e05a
except <except134@gmail.com>
2024-06-19 00:23:56 +0300

    Added additional lesson 2

--- Module1/Lesson6/main.py
+++ Module1/Lesson6/main.py
@@ -2,28 +2,30 @@
 print("Task 1")
 print("======")
 my_dict = {"Игорь": 1970, "Иван": 1980, "Сергей": 1990}
-print(my_dict)
+print("Dict:", my_dict)
 
-print(my_dict["Иван"])
-print(my_dict.get("Дмитрий"))
+print("Existing value:", my_dict["Иван"])
+print("Not existing value:", my_dict.get("Дмитрий"))
 
 my_dict["Дитрий"] = 1975
 my_dict["Андрей"] = 1985
-print(my_dict)
+print("New dict:", my_dict)
 
+for_delete = my_dict["Иван"]
 del my_dict["Иван"]
-print(my_dict)
+print("Deleted value:", for_delete)
+print("Modified dict:", my_dict)
 print()
 
 # Task 2
 print("Task 2")
 print("======")
 my_set = {2, 3, 4, 5, True, False, 4.5, "String", 3, 2, False, "String"}
-print(my_set)
+print("Set:", my_set)
 
 my_set.update([6, 7, 8])
-print(my_set)
+print("Modified set:", my_set)
 
 my_set.discard(7)
-print(my_set)
+print("Set after delete:", my_set)
 
--- Module1/LessonAdditional2/main.py
+++ Module1/LessonAdditional2/main.py
@@ -1,3 +1,4 @@
+# Task 1
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
