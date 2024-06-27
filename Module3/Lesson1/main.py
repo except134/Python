432225380df96170607dfd1cc65bd210b4bc8e77
except <except134@gmail.com>
2024-06-27 05:45:18 +0300

    Lesson 1 Module 3

--- Module3/Lesson1/main.py
+++ Module3/Lesson1/main.py
@@ -5,18 +5,15 @@ def count_calls():
     calls += 1
 
 def string_info(string):
-    ret = (len(string), string.upper(), string.lower())
     count_calls()
-    return ret
+    return (len(string), string.upper(), string.lower())
 
 def is_contains (string, list_to_search):
-    ret = False
+    count_calls()
     for i in range(len(list_to_search)):
         if string .lower() in list_to_search[i].lower() or list_to_search[i].lower() in string.lower():
-            ret = True
-            break
-    count_calls()
-    return ret
+            return True
+    return False
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
