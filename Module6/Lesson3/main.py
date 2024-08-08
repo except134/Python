6eefa6e605b165b361e3bb9e80f82bf2ce264440
except <except134@gmail.com>
2024-08-08 07:01:32 +0300

    Fixed Lesson3 Module6

--- Module6/Lesson3/main.py
+++ Module6/Lesson3/main.py
@@ -2,6 +2,7 @@ class Horse:
     def __init__(self):
         self.x_distance = 0
         self.sound = "Frrr"
+        super().__init__()
 
     def run(self, dx):
         self.x_distance += dx
@@ -16,8 +17,7 @@ class Eagle:
 
 class Pegasus(Horse, Eagle):
     def __init__(self):
-        Horse.__init__(self)
-        Eagle.__init__(self)
+        super().__init__()
 
     def move(self, dx, dy):
         self.run(dx)
