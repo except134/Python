cea565aee122299391b3abf53045b81d12884883
except <except134@gmail.com>
2024-08-08 06:52:08 +0300

    Fixed Lesson1 Module6

--- Module6/Lesson1/main.py
+++ Module6/Lesson1/main.py
@@ -1,43 +1,35 @@
 class Plant:
     edible = False
-    name = str()
 
     def __init__(self, name):
         self.name = name
-        self.edible = True
 
 class Flower(Plant):
     pass
 
 class Fruit(Plant):
-    pass
+    edible = True
 
 class Animal:
     alive = True
     fed = False
-    name = str()
 
     def __init__(self, name):
         self.name = name
-
-class Mammal(Animal):
+        
     def eat(self, food):
         if food.edible:
-            print(f"{self.name} съел {food.name}")
+            print(f'{self.name} съел {food.name}')
             self.fed = True
         else:
-            print(f"{self.name} не стал есть {food.name}")
+            print(f'{self.name} не стал есть {food.name}')
             self.alive = False
 
+class Mammal(Animal):
+    pass
+
 class Predator(Animal):
-    def eat(self, food):
-        if isinstance(food, Flower):
-            food.edible = False
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
-        else:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
+    pass
 
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
@@ -45,7 +37,7 @@ p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
 print(a1.name)
-print(a2.name)
+print(p1.name)
 
 print(a1.alive)
 print(a2.fed)
