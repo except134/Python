648b38dad583932cec9a728e49ff9ebcad70f3e7
except <except134@gmail.com>
2024-08-13 20:52:55 +0300

    Added Module4 snippets

---
+++ Module4/python_snippets/module1.py
@@ -0,0 +1,5 @@
+from module2 import say_hi as sh
+
+print('Привет я из модуля 1')
+
+sh()
---
+++ Module4/python_snippets/namespaces.py
@@ -0,0 +1,16 @@
+import math
+
+
+def square(x):
+    d = x ** 2
+    return d
+
+
+a = 5
+b = square(2)
+print(a)
+print(b)
+print(globals())
+
+
+
