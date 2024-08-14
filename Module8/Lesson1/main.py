995a39067a850c9a34a420aacf5dd68d64dd4b02
except <except134@gmail.com>
2024-08-14 03:38:27 +0300

    Added Lesson1 Module8

---
+++ Module8/Lesson1/main.py
@@ -0,0 +1,10 @@
+def add_everything_up(a, b):
+    try:
+        return round(a + b, 3)
+    except TypeError:
+        return str(a) + str(b)
+       
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+
