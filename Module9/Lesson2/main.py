399e5887cf23f1fcb1e46f278a26cf6dde9efd69
except <except134@gmail.com>
2024-08-26 19:03:48 +0300

    Fixed Lesson2 Module9

--- Module9/Lesson2/main.py
+++ Module9/Lesson2/main.py
@@ -3,7 +3,7 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 
 first_result = [len(s) for s in first_strings if len(s) >= 5]
 second_result = [(f, s) for f in first_strings for s in second_strings if len(f) == len(s)]
-third_result = [{s: len(s) for s in first_strings + second_strings if not len(s)%2}]
+third_result = {s: len(s) for s in first_strings + second_strings if not len(s)%2}
 
 print(first_result)
 print(second_result)
